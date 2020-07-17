6834141a744a4abc9ef48ac9ef033b74ee5db2cb
LyaShket <dingofleep@gmail.com>
2020-07-17 18:51:58 +0300

    Prototype: change player prefab, add mining mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,32 +5,84 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //[SerializeField]
-    [Header("Player Speed")]
-    [SerializeField] float horizontalSpeed = 3f;
+    [Header("Player Characteristics")]
+    [SerializeField] float horizontalSpeed = 20f;
     [SerializeField] float jumpForce = 180f;
     //private
     private Rigidbody2D rb;
+    private bool isMining = false;
 	private void Start()
 	{
         rb = GetComponent<Rigidbody2D>();
 	}
-	void Update()
+    void Update()
     {
-        // If player is jumping, horizontal speed 
-        float xSpeed = rb.velocity.y == 0 ? horizontalSpeed : horizontalSpeed / 1.3f;
-
-        // Left Move (Left Arrow)
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(new Vector2(-xSpeed, 0) * Time.deltaTime);
-        
-        // Right Move (Right Arrow)
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(new Vector2(xSpeed, 0) * Time.deltaTime);
-
-        // Jump (Up Arrow)
-        if (Input.GetKeyDown(KeyCode.UpArrow) && rb.velocity.y == 0)
+        if (!isMining)
         {
-            rb.AddForce(new Vector2(0, jumpForce));
+            // If player is jumping, horizontal speed 
+            float xSpeed = rb.velocity.y == 0 ? horizontalSpeed : horizontalSpeed / 4f;
+
+            // Limitation for velocity
+            if (rb.velocity.x > 2f)
+                rb.velocity = new Vector2(2f, rb.velocity.y);
+            if (rb.velocity.x < -2f)
+                rb.velocity = new Vector2(-2f, rb.velocity.y);
+
+            // Left Move (Left Arrow)
+            if (Input.GetKey(KeyCode.LeftArrow))
+                rb.AddForce(Vector2.left * xSpeed);
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+                rb.velocity = new Vector2(0, rb.velocity.y);
+
+            // Right Move (Right Arrow)
+            if (Input.GetKey(KeyCode.RightArrow))
+                rb.AddForce(Vector2.right * xSpeed);
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+                rb.velocity = new Vector2(0, rb.velocity.y);
+
+            // Jump (Up Arrow)
+            if (Input.GetKeyDown(KeyCode.UpArrow) && rb.velocity.y == 0)
+            {
+                rb.AddForce(new Vector2(0, jumpForce));
+            }
+        }
+        // Mine (Space)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isMining)
+			{
+                isMining = false;
+                StopCoroutine("Mine");
+            }
+            else
+			{
+                isMining = true;
+                StartCoroutine("Mine");
+            }
+        }
+    }
+    IEnumerator Mine()
+	{
+        while(true) {
+            yield return new WaitForSeconds(2f);
+
+            Vector2 raycastOrigin = transform.position;
+            raycastOrigin.y -= 0.3f;
+
+            RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, -Vector2.up);
+
+            if (hit.collider != null)
+            {
+                float distance = Mathf.Abs(hit.point.y - transform.position.y);
+
+                if (distance < 0.5f)
+                {
+                    Destroy(hit.collider.gameObject);
+
+                    Debug.Log(hit.collider.name);
+                    Debug.Log(distance);
+                }
+            }
         }
     }
 }
