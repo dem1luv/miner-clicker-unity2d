e146f1d1e30535c59b159f1a625b529243f6cd74
LyaShket <dingofleep@gmail.com>
2020-07-23 14:34:54 +0300

    Prototype: refactoring

--- Assets/Scripts/SaveScript.cs
+++ Assets/Scripts/SaveScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveScript : MonoBehaviour
+public static class SaveScript
 {
     public static GenerationBlock[] blocks;
     public static int money = 0;
